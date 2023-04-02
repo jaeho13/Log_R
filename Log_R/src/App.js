18b65b17be3f7696fb63269b6f30dd36d477e0f2
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-04-02 20:37:50 +0900

    Add files via upload

--- Log_R/src/App.js
+++ Log_R/src/App.js
@@ -1,11 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
 import LogIn from "./component/LogIn";
+import LogOut from "./component/LogOut";
 
 function App() {
   return (
     <div className="App">
-      <LogIn />
+      <nav>
+      </nav>
+      <Router>
+      <Routes>
+        <Route path="/login" element={<LogIn />}  />
+      </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
