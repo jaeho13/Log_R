55722d340e244464bf6705bf81ccedcf6727a989
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-04-12 00:52:50 +0900

    Add files via upload

--- Log_R/src/component/login/UserLogin.js
+++ Log_R/src/component/login/UserLogin.js
@@ -9,13 +9,15 @@ const UserLogin = props => {
 
     return (
         <form>
-            <label>ID</label>
-            <input id={`${Styled.username}`} type="text" placeholder="id" />
+            <label id={`${Styled.id}`}>이메일</label>
             <br></br>
-            <label>Password</label>
-            <input id={`${Styled.password}`} type="password" placeholder="password" />
+            <input id={`${Styled.username}`} type="text" placeholder="example@naver.com" />
             <br></br>
-            <button type="submit">회원가입</button>
+            <label id={`${Styled.pass}`}>비밀번호</label>
+            <br></br>
+            <input id={`${Styled.password}`} type="password" placeholder="비밀번호를 입력해주세요." />
+            <br></br>
+            <button id={`${Styled.join}`} type="submit">회원가입</button>
             <button type="submit">로그인</button>
         </form>
     );
